67a52cadbb837b99e40b7f6fd4e3effcce3a6c78
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-02-29 11:10:20 +0700

    refactor (style): configuration material-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+export default withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+})
 
