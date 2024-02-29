d9cbfbdf9dd61b1b1c77210946c9fcbf1efac076
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-02-29 11:13:17 +0700

    feat (route): add register route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Register from "../pages/Register";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
 
