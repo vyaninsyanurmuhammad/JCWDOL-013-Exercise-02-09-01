bba4ef7dab9992dcb6a8d548a2dff6927264286b
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-03-01 14:21:12 +0700

    feat (view): add badge for showing data length

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,32 +1,41 @@
-import { Navbar } from "@material-tailwind/react";
+import { Badge, Navbar } from "@material-tailwind/react";
 import { ReactNode } from "react";
 import { NavLink } from "react-router-dom";
+import { useAppSelector } from "../redux/hook";
 
 export default function layout({ children }: { children: ReactNode }) {
+  const registerState = useAppSelector((state) => state.registerReducer.users);
+
   return (
     <>
       <div className="w-screen h-screen overflow-hidden">
         <Navbar className="max-w-full">
           <div className="flex w-full justify-center">
             <div className="flex text-black gap-6">
-              <NavLink
-                to="/"
-                className={({ isActive, isPending, isTransitioning }) =>
-                  [
-                    isPending ? "pending" : "",
-                    isActive ? "text-gray-900 font-bold" : "text-gray-600 font-bold",
-                    isTransitioning ? "transitioning" : "",
-                  ].join(" ")
-                }
-              >
-                Users
-              </NavLink>
+              <Badge content={registerState.length}>
+                <NavLink
+                  to="/"
+                  className={({ isActive, isPending, isTransitioning }) =>
+                    [
+                      isPending ? "pending" : "",
+                      isActive
+                        ? "text-gray-900 font-bold"
+                        : "text-gray-600 font-bold",
+                      isTransitioning ? "transitioning" : "",
+                    ].join(" ")
+                  }
+                >
+                  Users
+                </NavLink>
+              </Badge>
               <NavLink
                 to="/register"
                 className={({ isActive, isPending, isTransitioning }) =>
                   [
                     isPending ? "pending" : "",
-                    isActive ? "text-gray-900 font-bold" : "text-gray-600 font-bold",
+                    isActive
+                      ? "text-gray-900 font-bold"
+                      : "text-gray-600 font-bold",
                     isTransitioning ? "transitioning" : "",
                   ].join(" ")
                 }
